6d42f6751e07fd66b6a14cc7229ace6986adc6ff
Santiago <developer.santi77@gmail.com>
2026-05-08 18:50:02 -0500

    fix: Se estaban duplicando los prefijos para las rutas de vehicles

--- src/infrastructure/http/routes/vehicle.routes.ts
+++ src/infrastructure/http/routes/vehicle.routes.ts
@@ -4,11 +4,11 @@ import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get('/vehicles', authMiddleware, VehicleController.listVehicles);
-router.post('/vehicles', authMiddleware, VehicleController.addVehicle);
-router.get('/vehicles/:id', authMiddleware, VehicleController.getVehicle);
-router.put('/vehicles/:id', authMiddleware, VehicleController.updateVehicle);
-router.delete('/vehicles/:id', authMiddleware, VehicleController.deleteVehicle);
-router.patch('/vehicles/:id/activate', authMiddleware, VehicleController.activateVehicle);
+router.get('/', authMiddleware, VehicleController.listVehicles);
+router.post('/', authMiddleware, VehicleController.addVehicle);
+router.get('/:id', authMiddleware, VehicleController.getVehicle);
+router.put('/:id', authMiddleware, VehicleController.updateVehicle);
+router.delete('/:id', authMiddleware, VehicleController.deleteVehicle);
+router.patch('/:id/activate', authMiddleware, VehicleController.activateVehicle);
 
 export default router;
